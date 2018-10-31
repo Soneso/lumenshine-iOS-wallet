9e38ce303e8cd1699ae250c880af50118bbbe86b
Razvan Chelemen <chelemen.razvan@gmail.com>
2018-10-31 11:35:05 +0200

    Available balance design

--- Lumenshine/Lumenshine/Classes/Views/Home/Info/Fees/NativeCurrencyFeeInfoViewController.swift
+++ Lumenshine/Lumenshine/Classes/Views/Home/Info/Fees/NativeCurrencyFeeInfoViewController.swift
@@ -15,15 +15,39 @@ class NativeCurrencyFeeInfoViewController: UIViewController {
     var wallet: FundedWallet!
     
     private let userManager = UserManager()
-    private let xlmAccountReserve = "1.0 XLM - account reserve"
-    private let newLine = "\n"
-    
+    private let xlmAccountReserve = "1.0 XLM"
+
+    @IBOutlet weak var stackViewBackgroundView: UIView!
+    @IBOutlet weak var trustLinesView: UIView!
+    @IBOutlet weak var signersView: UIView!
+    @IBOutlet weak var dataEntriesView: UIView!
+    @IBOutlet weak var offersView: UIView!
+    @IBOutlet weak var stackViewSeparatorView: UIView!
     @IBOutlet weak var liabilitiesView: UIView!
     
-    @IBOutlet weak var minimumBalanceLabel: UILabel!
+    @IBOutlet weak var totalReservesValueLabel: UILabel!
+    @IBOutlet weak var networkReserveValueLabel: UILabel!
+    @IBOutlet weak var trustlinesValueLabel: UILabel!
+    @IBOutlet weak var signersValueLabel: UILabel!
+    @IBOutlet weak var dataEntriesValueLabel: UILabel!
+    @IBOutlet weak var offersValueLabel: UILabel!
+    @IBOutlet weak var networkReserveLabel: UILabel!
+    @IBOutlet weak var trustlinesLabel: UILabel!
+    @IBOutlet weak var signersLabel: UILabel!
+    @IBOutlet weak var dataEntriesLabel: UILabel!
+    @IBOutlet weak var offersLabel: UILabel!
+    @IBOutlet weak var availableBalanceLabel: UILabel!
     @IBOutlet weak var liabilitiesLabel: UILabel!
     @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var liabilitiesInfoLabel: UILabel!
+    @IBOutlet weak var liabilitiesValueLabel: UILabel!
     
+    @IBOutlet weak var dismissButton: UIButton!
+    
+    @IBAction func dismissButtonAction(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -40,28 +64,44 @@ class NativeCurrencyFeeInfoViewController: UIViewController {
     }
     
     private func setupView() {
-        view.backgroundColor = Stylesheet.color(.veryLightGray)
+        view.backgroundColor = Stylesheet.color(.lightGray)
+        stackViewSeparatorView.backgroundColor = Stylesheet.color(.lightGray)
+        stackViewBackgroundView.backgroundColor = Stylesheet.color(.helpButtonGray)
+        dismissButton.backgroundColor = Stylesheet.color(.lightGray)
+        dismissButton.setTitleColor(Stylesheet.color(.lightBlack), for: .normal)
+        dismissButton.borderWidthPreset = .border1
+        dismissButton.borderColor = Stylesheet.color(.lightBlack)
         
         if let liabilities = CoinUnit(currency.sellingLiabilities) {
             if liabilities.isEqual(to: 0) {
                 liabilitiesView.isHidden = true
+                liabilitiesInfoLabel.text = nil
             } else {
                 let liabilitiesValue = "\(liabilities) XLM"
-                addValueLine(toLabel: liabilitiesLabel, value: liabilitiesValue)
+                liabilitiesValueLabel.text = liabilitiesValue
             }
         }
     }
     
     private func setupLabels() {
-        addValueLine(toLabel: minimumBalanceLabel, value: xlmAccountReserve)
+        setupUINetworkReserved()
+        availableBalanceLabel.text = "\(wallet.nativeBalance) XLM"
         
         userManager.getAccountDetails(forAccountID: wallet.publicKey) { (response) -> (Void) in
             switch response {
             case .success(details: let accountDetails):
                 let trustlines = self.checkNumberOfTrustlines()
+                self.setupUI(forItem: trustlines, forView: self.trustLinesView, forLabel: self.trustlinesLabel, forValueLabel: self.trustlinesValueLabel)
+                
                 let signers = self.checkNumberOfSigners(forAccount: accountDetails)
+                self.setupUI(forItem: signers, forView: self.signersView, forLabel: self.signersLabel, forValueLabel: self.signersValueLabel)
+                
                 let dataEntries = self.checkNumberOfDataEntries(forAccount: accountDetails)
+                self.setupUI(forItem: dataEntries, forView: self.dataEntriesView, forLabel: self.dataEntriesLabel, forValueLabel: self.dataEntriesValueLabel)
+                
                 let offers = self.checkNumberOfOffers(nrOfTrustLines: trustlines.count, nrOfSigners: signers.count, nrOfDataEntries: dataEntries.count)
+                self.setupUI(forItem: offers, forView: self.offersView, forLabel: self.offersLabel, forValueLabel: self.offersValueLabel)
+                
                 self.calculateTotal(trustlines: trustlines.value, signers: signers.value, dataEntries: dataEntries.value, offers: offers.value)
             case .failure(error: let error):
                 print(error.localizedDescription)
@@ -69,23 +109,13 @@ class NativeCurrencyFeeInfoViewController: UIViewController {
         }
     }
     
-    private func addValueLine(toLabel label: UILabel, value: String) {
-        if label.text?.isEmpty == false {
-            label.text?.append(newLine)
-        }
-        
-        label.text?.append(value)
-    }
-    
     private func checkNumberOfTrustlines() -> (count: Int, value: CoinUnit) {
         if wallet.balances.count > 1 {
             let nrOFTrustLines = wallet.balances.count - 1
             let amountValue = CoinUnit(nrOFTrustLines) * CoinUnit.Constants.baseReserver
-            let value = "\(amountValue) XLM - \(nrOFTrustLines) \(nrOFTrustLines == 1 ? "trustline" : "trustlines") to other \(nrOFTrustLines == 1 ? "currency" : "currencies")"
-            addValueLine(toLabel: minimumBalanceLabel, value: value)
             return (nrOFTrustLines, amountValue)
         }
-        
+
         return (0, 0)
     }
     
@@ -93,33 +123,27 @@ class NativeCurrencyFeeInfoViewController: UIViewController {
         if account.signers.count > 1 {
             let nrOfSigners = account.signers.count - 1
             let amountValue = CoinUnit(nrOfSigners) * CoinUnit.Constants.baseReserver
-            let value = "\(amountValue) XLM - \(nrOfSigners) additional \(nrOfSigners == 1 ? "signer" : "signers")"
-            addValueLine(toLabel: minimumBalanceLabel, value: value)
             return (nrOfSigners, amountValue)
         }
-        
+
         return (0, 0)
     }
     
     private func checkNumberOfDataEntries(forAccount account: AccountResponse) -> (count: Int, value: CoinUnit) {
         if account.data.count > 0 {
             let amountValue = CoinUnit(account.data.count) * CoinUnit.Constants.baseReserver
-            let value = "\(amountValue) XLM - \(account.data.count) data \(account.data.count == 1 ? "entry" : "entries")"
-            addValueLine(toLabel: minimumBalanceLabel, value: value)
             return (account.data.count, amountValue)
         }
-        
+
         return (0, 0)
     }
     
     private func checkNumberOfOffers(nrOfTrustLines: Int, nrOfSigners: Int, nrOfDataEntries: Int) -> (count: Int, value: CoinUnit) {
         let minimumAccountBalanceWithoutOffers = CoinUnit((2 + (nrOfTrustLines + nrOfSigners + nrOfDataEntries))) * CoinUnit.Constants.baseReserver
         let nrOfOffers = Int((CoinUnit.minimumAccountBalance(forWallet: wallet) - minimumAccountBalanceWithoutOffers) / CoinUnit.Constants.baseReserver)
-        
+
         if nrOfOffers > 0 {
             let amountValue = CoinUnit(nrOfOffers) * CoinUnit.Constants.baseReserver
-            let value = "\(amountValue) XLM - \(nrOfOffers) \(nrOfOffers == 1 ? "offer" : "offers")"
-            addValueLine(toLabel: minimumBalanceLabel, value: value)
             return (nrOfOffers, amountValue)
         }
         
@@ -131,7 +155,21 @@ class NativeCurrencyFeeInfoViewController: UIViewController {
         let additionalAmount = trustlines + signers + dataEntries + offers
         let totalValue = additionalAmount + (liabilities ?? 0) + 1
         let total = "\(totalValue) XLM"
-        addValueLine(toLabel: totalAmountLabel, value: total)
+        totalReservesValueLabel.text = total
+    }
+    
+    private func setupUI(forItem item:(count: Int, value: CoinUnit), forView view: UIView, forLabel titleLabel: UILabel, forValueLabel valueLabel: UILabel) {
+        if item.count == 0 {
+            view.isHidden = true
+            return
+        }
+        
+        titleLabel.text?.append(" (\(item.count))")
+        valueLabel.text = "\(item.value) XLM"
+    }
+    
+    private func setupUINetworkReserved() {
+        networkReserveValueLabel.text = xlmAccountReserve
     }
     
     @objc func closeAction() {
@@ -139,8 +177,8 @@ class NativeCurrencyFeeInfoViewController: UIViewController {
     }
     
     private func setupNavigationItem() {
-        navigationItem.titleLabel.text = "Available XLM"
-        navigationItem.titleLabel.textColor = Stylesheet.color(.blue)
+        navigationItem.titleLabel.text = "Minimum balance"
+        navigationItem.titleLabel.textColor = Stylesheet.color(.lightBlack)
         navigationItem.titleLabel.font = R.font.encodeSansSemiBold(size: 15)
         
         let backButton = Material.IconButton()
